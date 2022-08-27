f5d06c709e0a81b970682c8ce80bea6bcab8f535
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2022-08-27 18:30:20 +0300

    progress update

--- src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -3,7 +3,10 @@ package com.luv2code.springdemo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,16 +16,33 @@ import com.luv2code.springdemo.entity.Student;
 @RequestMapping("/api")
 public class StudentRestController {
 	
-	//define endpoint
-	@GetMapping("/students")
-	public List<Student> getStudents(){
+	private List<Student> studentList;
+	
+	//define @PostConstruct to load the student data
+	@PostConstruct
+	public void loadData() {
 		
-		List<Student> studentList = new ArrayList<>(); 
-		
+		studentList = new ArrayList<>();
 		studentList.add(new Student("Mary", "Stuart")); 
 		studentList.add(new Student("Jane", "Doe"));
 		studentList.add(new Student("Alex", "Motiloff"));
+	}
+	
+	//define endpoint for all students
+	@GetMapping("/students")
+	public List<Student> getStudents(){
 		
 		return studentList;
+	}
+	
+	//define endpoint for a single student
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId) {
+		
+		if (studentId<=studentList.size()-1) {
+			return studentList.get(studentId);
+		}
+		
+		return null;
 	}
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -3,5 +3,8 @@
 Spring REST Demo
 <hr>
 <a href="test/hello">Hello</a>
+<br>
+<a href="api/students">Get all students</a>
+
 </body>
 </html>
